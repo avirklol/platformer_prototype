0766aa123c5fbbc071331633e21c15296765bbac
Annand Virk <annandvirk@gmail.com>
2025-07-03 14:50:04 -0400

    Adjusted wall jump mechanics by modifying jump force calculations for both vertical and horizontal velocities. This change reduces the jump force applied when colliding with walls, enhancing control during wall jumps.

--- scripts/states/wall_jump.gd
+++ scripts/states/wall_jump.gd
@@ -1,19 +1,17 @@
 extends State
 
-@export var jump_force: float = 150.0
 @export var falling_state: State
 @export var wall_slide_state: State
 
-
 func enter() -> void:
 	super()
-	parent.velocity.y = -jump_force * 3
+	parent.velocity.y = -jump_force * 2
 	if %WallBodyCheck.get_collision_normal(0)[0] < 0:
-		parent.velocity.x = -jump_force
+		parent.velocity.x = -jump_force / 2
 		flip_animations(true)
 		flip_collision_shapes(true)
 	else:
-		parent.velocity.x = jump_force
+		parent.velocity.x = jump_force / 2
 		flip_animations(false)
 		flip_collision_shapes(false)
 
