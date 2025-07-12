20552352bc122bf15edd9f4507c31312f7dae316
Annand Virk <annandvirk@gmail.com>
2025-07-12 13:06:57 -0400

    Introduced a new chest container script and scene, enabling item spawning and interaction mechanics. Implemented an item pickup script and scene to manage item collection by players. Enhanced item management with attributes like rarity and weight, integrating with the existing item database for dynamic loot generation.

---
+++ interactibles/items/containers/chest.gd
@@ -0,0 +1,61 @@
+extends Area2D
+
+@onready var animations: AnimatedSprite2D = %AnimatedSprite2D
+# @onready var endless_timer: Timer = %EndlessTimer
+@onready var item_pickup: PackedScene = preload("res://interactibles/items/pickups/item_pick_up.tscn")
+
+@export var item_type: String
+@export var item_name: String
+@export var item_rarity: int
+@export var loot_amount: int = 1
+
+var player_in_range: bool = false
+var opened: bool = false
+
+
+func _ready() -> void:
+	animations.play("closed")
+	animations.animation_finished.connect(_on_animation_finished)
+	body_entered.connect(_on_body_entered)
+	body_exited.connect(_on_body_exited)
+	# endless_timer.timeout.connect(_on_timeout)
+
+
+func _on_timeout() -> void:
+	spawn_loot()
+	# endless_timer.start()
+
+func _on_animation_finished() -> void:
+	if animations.name == "open" or animations.name == "open_empty":
+		animations.play("opened")
+
+
+func _on_body_entered(body: Node2D) -> void:
+	if body.is_in_group("characters"):
+		player_in_range = true
+
+
+func _on_body_exited(body: Node2D) -> void:
+	if body.is_in_group("characters"):
+		player_in_range = false
+
+
+func _process(delta: float) -> void:
+	if player_in_range and !opened:
+		if Input.is_action_just_pressed("interact"):
+			animations.play("open")
+			opened = true
+			spawn_loot()
+			# endless_timer.start()
+
+
+func spawn_loot() -> void:
+	for i in range(loot_amount):
+		var item: ItemData = item_database.get_item(item_type, item_name, item_rarity)
+		if item:
+			var loot: Node2D = item_pickup.instantiate()
+			loot.item = item.duplicate(true)
+			loot.position = position
+			loot.z_index = 0
+			get_tree().current_scene.add_child(loot)
+			loot.pop_out()
---
+++ interactibles/items/pickups/item_pick_up.gd
@@ -0,0 +1,41 @@
+extends RigidBody2D
+
+@onready var rigid_collision_shape: CollisionShape2D = %RigidCollisionShape
+@onready var area_collision_shape: CollisionShape2D = %AreaCollisionShape
+@onready var pickup_area: Area2D = %PickUpArea
+@onready var animations: AnimatedSprite2D = %ItemAnimation
+
+@export var item: ItemData
+@export var collision_shape_size: Vector2 = Vector2(16, 16)
+
+
+func _ready() -> void:
+	mass = item.weight
+	rigid_collision_shape.shape.size = collision_shape_size
+	area_collision_shape.shape.size = collision_shape_size + Vector2(1, 1)
+	area_collision_shape.disabled = true
+	if item.sprite_sheet:
+		animations.sprite_frames = item.sprite_sheet
+		animations.play("default")
+	pickup_area.body_entered.connect(_on_body_entered)
+
+
+func _on_body_entered(body: Node2D) -> void:
+	if body.is_in_group("characters"):
+		body.inventory.add_item(item)
+		queue_free()
+
+
+func _physics_process(delta: float) -> void:
+	if linear_velocity.y > 0:
+		z_index = 1
+
+	if linear_velocity.y == 0:
+		area_collision_shape.disabled = false
+		set_collision_mask_value(2, true)
+
+
+func pop_out() -> void:
+	var x_impulse: Vector2 = Vector2(randf() * 200 - 150, -350)
+	apply_impulse(x_impulse, Vector2.ZERO)
+	set_collision_mask_value(2, false)
