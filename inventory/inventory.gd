bc103aee7a50c47176bc296cd8970db4dbe36097
Annand Virk <annandvirk@gmail.com>
2025-07-12 13:07:37 -0400

    Introduced a new inventory slot script and scene, enabling dynamic item management within the inventory. Implemented a test key resource with associated textures and animations for item representation. Updated inventory scene layout for improved usability and adjusted item handling logic to support stacking and visibility management.

--- inventory/inventory.gd
+++ inventory/inventory.gd
@@ -1,30 +1,39 @@
 extends Node
 class_name Inventory
 
-@export var items: Array[InventoryItem] = []
-var default_position: Vector2 = Vector2(0, 0)
+@onready var inventory_items: Array = %GridContainer.get_children()
+
 
 func _ready() -> void:
 	self.visible = false
-	default_position = self.position
 
-func add_item(item: InventoryItem):
-	items.append(item)
 
-func remove_item(item: InventoryItem):
-	items.erase(item)
+func add_item(added_item: ItemData):
+	for slot in inventory_items:
+		if slot.item == null:
+			slot.item = added_item
+			return
+		else:
+			if slot.item.name == added_item.name and slot.item.stackable:
+				slot.item.amount += added_item.amount
+				return
+			else:
+				pass
+	print("Inventory is full")
+
+
+func remove_item(removed_item: ItemData):
+	for slot in inventory_items:
+		if slot.item == removed_item:
+			slot.update_slot(null)
+			return
+
 
 func _process(delta: float) -> void:
 	if Input.is_action_just_pressed("inventory"):
 		self.visible = !self.visible
-		self.position = default_position
-
-	if self.visible:
-		if Input.is_action_pressed("move_left"):
-			self.position.x -= 10
-		if Input.is_action_pressed("move_right"):
-			self.position.x += 10
-		if Input.is_action_pressed("move_up"):
-			self.position.y -= 10
-		if Input.is_action_pressed("move_down"):
-			self.position.y += 10
+		if self.visible:
+			print("Inventory contains:")
+			for slot in inventory_items:
+				if slot.item:
+					print(str(slot.item.amount) + " " + str(slot.item.name))
---
+++ inventory/inventory_slot.gd
@@ -0,0 +1,24 @@
+extends Panel
+
+@onready var background: Sprite2D = %SlotBackground
+@onready var item_image: Sprite2D = %ItemImage
+@onready var item_count: Label = %StackCount
+
+var active_slot: Texture2D = preload("res://assets/used/ui/Grid/Black/GridSlotC.png")
+var inactive_slot: Texture2D = preload("res://assets/used/ui/Grid/Black/GridSlotInactive.png")
+var item: ItemData = null
+
+
+func _process(delta: float) -> void:
+	update_slot(item)
+
+
+func update_slot(current_item: ItemData):
+	if !current_item:
+		item_image.visible = false
+		background.texture = inactive_slot
+	else:
+		item_image.visible = true
+		item_image.texture = current_item.icon
+		item_count.text = str(current_item.amount)
+		background.texture = active_slot
