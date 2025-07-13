e873ceccf4d892ce02a501ff15d465795d3d68b4
Annand Virk <annandvirk@gmail.com>
2025-07-13 13:59:54 -0400

    Refactored player scene by removing debug nodes and reorganizing audio nodes. Updated state machine initialization to include audio references for body and voice audio.

--- player/states/state_machine.gd
+++ player/states/state_machine.gd
@@ -9,12 +9,14 @@ var next_state: State
 
 # Initialize the state machine by giving each child state a reference to the
 # parent object it belongs to and enter the default starting_state.
-func init(parent: CharacterBody2D, animations: AnimatedSprite2D, effects: AnimatedSprite2D, input_handler: Node) -> void:
+func init(parent: CharacterBody2D, animations: AnimatedSprite2D, effects: AnimatedSprite2D, input_handler: Node, body_audio: AudioStreamPlayer2D, voice_audio: AudioStreamPlayer2D) -> void:
 	for child in get_children():
 		child.parent = parent
 		child.animations = animations
 		child.effects = effects
 		child.input_handler = input_handler
+		child.body_audio = body_audio
+		child.voice_audio = voice_audio
 		child.animations.animation_finished.connect(child._on_animation_finished)
 		%LadderRelease.timeout.connect(child._on_ladder_release_timeout)
 
