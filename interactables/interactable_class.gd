1ca3ea584758c37ac52c1713ac9c61aae9176701
Annand Virk <annandvirk@gmail.com>
2025-07-23 14:09:02 -0400

    Added Interactable class for player interaction handling, including body detection and interaction menu management.

---
+++ interactables/interactable_class.gd
@@ -0,0 +1,33 @@
+extends Area2D
+class_name Interactable
+
+@export var interaction_data: InteractionData
+
+var player: CharacterBody2D = null
+var interaction_active: bool = false
+var interaction_menu: InteractionMenu = null
+
+
+func _ready() -> void:
+	body_entered.connect(_on_body_entered)
+	body_exited.connect(_on_body_exited)
+
+
+func _on_body_entered(body: Node2D) -> void:
+	if body.is_in_group("characters"):
+		player = body
+
+
+func _on_body_exited(body: Node2D) -> void:
+	if body.is_in_group("characters"):
+		player = null
+
+func open_interaction() -> void:
+	pass
+
+
+func close_interaction() -> void:
+	interaction_menu.visible = false
+	interaction_menu.destroy_interaction_menu()
+	interaction_active = false
+	interaction_menu = null
