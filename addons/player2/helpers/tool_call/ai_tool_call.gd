6a3e0f15f1a9460c4e868aef216c71c9eb567ebf
Annand Virk <annandvirk@gmail.com>
2025-08-03 22:08:21 -0400

    Delete addons/player2 directory

--- addons/player2/helpers/tool_call/ai_tool_call.gd
+++
@@ -1,6 +0,0 @@
-class_name AIToolCall
-extends Resource
-
-@export var function_name : String
-@export var description : String
-@export var args : Array[AIToolCallParameter]
--- addons/player2/helpers/tool_call/ai_tool_call_reply.gd
+++
@@ -1,6 +0,0 @@
-class_name AIToolCallReply
-extends Resource
-
-@export var function_name : String
-@export var args : Dictionary
-@export var optional_message : String
