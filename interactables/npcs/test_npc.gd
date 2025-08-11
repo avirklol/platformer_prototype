edda2b8074a58c59fac30ea78c9805e4b5122d4c
Annand Virk <annandvirk@gmail.com>
2025-08-11 19:23:59 -0400

    Added interaction_states array to clean up functionality.

--- interactables/npcs/test_npc.gd
+++ interactables/npcs/test_npc.gd
@@ -1,5 +1,7 @@
 extends NPC
 
+@export var interaction_states: PackedStringArray = ["Standing", "Walking", "Crouching", "CrouchWalking"]
+
 var times_yelled: int = 0
 var response_message: String = ""
 
@@ -18,10 +20,10 @@ func _process(delta: float) -> void:
 		interaction_menu.global_position = position + Vector2(-interaction_menu.size.x / 2, -40)
 		if Input.is_action_just_pressed("interact"):
 			if !interaction_menu.visible:
-				if player.state_machine.current_state.name in ["Standing", "Walking", "Crouching", "CrouchWalking"]:
+				if player.state_machine.current_state.name in interaction_states:
 					response_message = ""
 					interaction_menu.create_interaction_menu(interaction_menu.Type.NPC, 0, [])
-					brain.make_request("Hello, how are you?")
+					brain.process_conversation("Hello, how are you?")
 					interaction_menu.visible = true
 					interaction_active = true
 			else:
